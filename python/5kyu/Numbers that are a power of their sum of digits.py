77ec58157a8d6d892a43f445a19661b1f8446d18
Fyo <fyosaadati@gmail.com>
2024-11-03 13:44:59 +0330

    style: fix code style

--- python/5kyu/Numbers that are a power of their sum of digits.py
+++ python/5kyu/Numbers that are a power of their sum of digits.py
@@ -1,14 +1,17 @@
 """ 
 (5kyu)
-The number 81 has a special property, a certain power of the sum of its digits is equal to 81 (nine squared). 
-Eighty one (81), is the first number in having this property (not considering numbers of one digit). 
+The number 81 has a special property, a certain power of the sum of its digits 
+is equal to 81 (nine squared). 
+Eighty one (81), is the first number in having this property (not considering 
+numbers of one digit). 
 The next one, is 512. Let's see both cases with the details
 
 8 + 1 = 9 and 92 = 81
 
 512 = 5 + 1 + 2 = 8 and 83 = 512
 
-We 13:52521875, need to make a function that receives a number as argument n and returns the n-th term of this sequence of numbers.
+We 13:52521875, need to make a function that receives a number as argument n 
+and returns the n-th term of this sequence of numbers.
 """
 def power_sumDigTerm(n):
     dict = {1:81, 2:512, 3:2041, 4:4913, 5:5832, 
